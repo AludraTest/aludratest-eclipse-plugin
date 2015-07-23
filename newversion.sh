243e581c49b7fec43af89922e84d1196cd36a40d
Florian Albrecht <Florian.Albrecht@hamburgsud.com>
2015-07-23 11:43:36 +0200

    Updated to next release 0.6.0

--- newversion.sh
+++ newversion.sh
@@ -2,8 +2,8 @@
 #
 
 # update version here and run script
-PREV_VERSION=0.5.3
-NEW_VERSION=0.5.4
+PREV_VERSION=0.5.4
+NEW_VERSION=0.6.0
 
 PREV_VERSION_SHORT=$(echo $PREV_VERSION | sed s/\\.//g)
 NEW_VERSION_SHORT=$(echo $NEW_VERSION | sed s/\\.//g)
